22d895d83bc515f3a8ff31ada579cff288db646b
Mugisha Moses <moses@appfrica.org>
2011-09-06 10:20:41 +0300

    staic content stuff

--- coffeeclub_project/coffeeclubapp/models.py
+++ coffeeclub_project/coffeeclubapp/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class MenuItem(models.Model):
+    name=models.CharField(max_length=50,blank=True)
+    cost=models.IntegerField(max_length=10,blank=True,null=True)
+
+class Customer(models.Model):
+    #user=models.ForeignKey(Contact,related_name="customer")
+    running_order=models.BooleanField(default=False)
+
+class Order(models.Model):
+    customer=models.ForeignKey(Customer)
+
+class Account(models.Model):
+    pass
--- coffeeclub_project/urls.py
+++ coffeeclub_project/urls.py
@@ -1,10 +1,36 @@
 from django.conf.urls.defaults import *
 from coffeeclubapp.views import dashboard
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from rapidsms_httprouter.urls import urlpatterns as router_urls
+from django.conf import settings
+
+
+from generic.urls import urlpatterns as generic_urls
 
 urlpatterns = patterns('',
 
-                        url(r'^$', dashboard ,name="coffee-dashboard"),
-)
+        url(r'^$', dashboard ,name="coffee-dashboard"),
+        (r'^account/', include('rapidsms.urls.login_logout')),
+            url('^accounts/login', 'rapidsms.views.login'),
+            url('^accounts/logout', 'rapidsms.views.logout'),
+            # RapidSMS contrib app URLs
+            (r'^ajax/', include('rapidsms.contrib.ajax.urls')),
+            (r'^export/', include('rapidsms.contrib.export.urls')),
+            (r'^httptester/', include('rapidsms.contrib.httptester.urls')),
+            (r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
+            (r'^messaging/', include('rapidsms.contrib.messaging.urls')),
+            (r'^registration/', include('auth.urls')),
+            (r'^scheduler/', include('rapidsms.contrib.scheduler.urls')),
+            (r'^polls/', include('poll.urls')),
+        ) + router_urls  + generic_urls
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        # helper URLs file that automatically serves the 'static' folder in
+        # INSTALLED_APPS via the Django static media server (NOT for use in
+        # production)
+        (r'^', include('rapidsms.urls.static_media')),
+    )
+
+from rapidsms_httprouter.router import get_router
+get_router()
