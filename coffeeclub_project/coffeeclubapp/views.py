7ff4c6c3e7dad4259d270fe3d72cc5fb57caba0e
Mugisha Moses <moses@appfrica.org>
2011-09-07 11:02:00 +0300

    context instance

--- coffeeclub_project/coffeeclubapp/views.py
+++ coffeeclub_project/coffeeclubapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def dashboard(request):
 
-    return render_to_response('coffeeclubapp/dashboard.html')
+    return render_to_response('coffeeclubapp/dashboard.html',context_instance=RequestContext(request))
 
 def cutomer_detail(request):
-    return render_to_response('coffeeclubapp/customer_detail.html')
+    return render_to_response('coffeeclubapp/customer_detail.html',context_instance=RequestContext(request))
 
