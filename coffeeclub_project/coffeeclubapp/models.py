7b1932db7aa206433e6429aee7eef42920890219
Assey Alfred <asseym@gmail.com>
2011-09-08 17:25:37 +0300

    clean up

--- coffeeclub_project/coffeeclubapp/models.py
+++ coffeeclub_project/coffeeclubapp/models.py
@@ -35,9 +35,10 @@ class Customer(Contact):
     email = models.EmailField(blank=True, null=True)
 
     def in_negative(self):
-        return Account.objects.filter(owner=self, balance_lt=0)
+#        return Account.objects.filter(owner=self, balance_lt=0)
+        return self.accounts.filter(balance_lt=0)
 
-    def bal(self):
+    def account_bal(self):
         return self.accounts.all()[0].balance
 
     def send_email(self, context={}, type=False):
