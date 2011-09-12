4d68c68c919a8cf1e2c7f9cad95a96a73d9affed
Assey Alfred <asseym@gmail.com>
2011-09-12 11:18:46 +0300

    added ability to quit the coffee club by sending keyword quit or stop etc

--- coffeeclub_project/coffeeclubapp/app.py
+++ coffeeclub_project/coffeeclubapp/app.py
@@ -1,13 +1,33 @@
 from rapidsms.apps.base import AppBase
 from script.models import Script, ScriptProgress
-from rapidsms.models import Contact
+from django.conf import settings
+from unregister.models import Blacklist
+from coffeeclubapp.models import Customer
 
 class App (AppBase):
 
     def handle (self, message):
-        if not message.connection.contact and \
-           not ScriptProgress.objects.filter(script__slug='coffee_autoreg', connection=message.connection).count():
-            ScriptProgress.objects.create(script=Script.objects.get(slug="coffee_autoreg"), \
+        if message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_OUT_WORDS', [])]:
+            Blacklist.objects.create(connection=message.connection)
+            if (message.connection.contact):
+                customer = Customer.objects.get(pk=message.connection.contact.pk)
+                message.connection.contact.active = False
+                message.connection.contact.save()
+                customer.active = False
+                customer.save()
+            message.respond(getattr(settings, 'OPT_OUT_CONFIRMATION', 'Thank you for using being a part of the Coffee Club. If you ever want to rejoin the Coffee Club send JOIN to 6767'))
+            return True
+        elif message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_IN_WORDS', [])]:
+            if Blacklist.objects.filter(connection=message.connection).count() or not message.connection.contact:
+                for b in Blacklist.objects.filter(connection=message.connection):
+                    b.delete()
+                if not message.connection.contact and \
+                not ScriptProgress.objects.filter(script__slug='coffee_autoreg', connection=message.connection).count():
+                    ScriptProgress.objects.create(script=Script.objects.get(slug="coffee_autoreg"), \
                                           connection=message.connection)
+            else:
+                message.repond("You are already in the system and do not need to 'Join' again.")
+            return True
+        elif Blacklist.objects.filter(connection=message.connection).count():
             return True
         return False
--- coffeeclub_project/coffeeclubapp/utils.py
+++ coffeeclub_project/coffeeclubapp/utils.py
@@ -6,7 +6,7 @@ Created on Sep 8, 2011
 from coffeeclubapp.models import Customer, Account
 
 def balance_alerts(bal=False, subj=False, signature=False):
-    customers = Customer.objects.all()
+    customers = Customer.objects.filter(active=True)
     for customer in customers:
         if customer.accounts.all()[0].balance < 0:
             bal = bal if bal else Account.objects.filter(customer=customer)[0].balance
@@ -16,7 +16,7 @@ def balance_alerts(bal=False, subj=False, signature=False):
             customer.send_email(ctxt)
 
 def marketing_email(subject=False, message=False):
-    customers = Customer.objects.all()
+    customers = Customer.objects.filter(active=True)
     for customer in customers:
         subject = subject if subject else 'Buy 1 get 4 free!'
         message = message if message else 'This September buy 1 expresso and get 4 on the house!'
